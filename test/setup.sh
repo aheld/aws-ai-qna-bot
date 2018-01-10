d8334364e477b6f260c5178601ee340647869176
John M Calhoun <johnmcalhoun123@gmail.com>
2018-01-09 22:41:10 -0500

    small

--- test/setup.sh
+++ test/setup.sh
@@ -3,18 +3,13 @@ __dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE=$__dirname/..
 cd $BASE
 
-cd $BASE & make bootstrap
-npm run --silent stack dev/bootstrap make-sure -- \
-    --no-check          \
-    --no-interactive    \
-    --verbose           \
-    --local             
+npm run --silent bootstrap
 
 if [ $? -ne 0 ]; then
     echo "failed to create bootstrap bucket"
     exit 1
 fi
-npm run --silent upload
+
 rm $__dirname/debug.log;
 rm $__dirname/output.log;
 rm $__dirname/output.json;
